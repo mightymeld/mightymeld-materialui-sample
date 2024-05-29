2ce0dcf50b0096b13208bb61fb651a1fa5a70711
tim <tim@mightymeld.com>
2024-05-29 16:49:23 +0200

    updates mightymeld package to 0.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const conditionalPlugins = [];
-
-if (process.env.MIGHTYMELD) {
-  conditionalPlugins.push("@mightymeld/runtime/babel-plugin-mightymeld");
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [...conditionalPlugins],
-      },
-    }),
+    react(),
   ],
 });
